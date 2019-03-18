3748a1ee4d0b6f32cb7cb53e16787d88b0c6ea9d
ycnalin <ycnalin@gmail.com>
2019-03-19 00:03:24 +0800

    add GlobalSolver class

--- src/com/huawei/data/RoadMap.java
+++ src/com/huawei/data/RoadMap.java
@@ -17,75 +17,81 @@ import java.util.TreeMap;
 import org.apache.log4j.Logger;
 
 public class RoadMap {
-	
+
 	private static final Logger logger = Logger.getLogger(RoadMap.class);
-	
+
 	public static Map<Integer, Cross> crosses = new HashMap<>();
 	public static Map<Integer, Road> roads = new HashMap<>();
 	public static Map<Integer, Car> cars = new HashMap<>();
-	
+
 	// ordered sequence from min to max
 	public static List<Integer> crossSequence = new ArrayList<>();
-	
+
 	public static int nowTime = 0;
 	public static List<Car> outRoadCars = new LinkedList<>();
 	public static Set<Car> nowRunCars = new HashSet<>();
-	public static Set<Car> nowWaitedCars = new HashSet<>();
 	public static Set<Car> finishCars = new HashSet<>();
-	
+
+	public static Set<Car> nowWaitedCars = new HashSet<>();
+
 	public static void runMapWithOutView() {
 		initMap();
-		while(finishCars.size()!=cars.size()) {
+		while (finishCars.size() != cars.size()) {
 			updateMap();
 			nowTime++;
 		}
 	}
-	
+
 	public static void initMap() {
 		logger.info("init map in " + nowTime);
-		for(Car i : cars.values())
+		for (Car i : cars.values())
 			outRoadCars.add(i);
 		Collections.sort(outRoadCars, new Comparator<Car>() {
 			@Override
 			public int compare(Car o1, Car o2) {
-				return o1.getCarId()-o2.getCarId();
+				return o1.getCarId() - o2.getCarId();
 			}
 		});
 	}
-	
+
 	public static void updateMap() {
 		logger.info("start updateMap in " + nowTime);
 		nowWaitedCars.addAll(nowRunCars);
-		for(Car car : nowWaitedCars) {
+		for (Car car : nowWaitedCars) {
 			car.setUpdated(false);
 		}
 		// step one
 		// only consider and update cars that will not pass the cross
 		Iterator<Map.Entry<Integer, Road>> iterator = roads.entrySet().iterator();
 		while (iterator.hasNext()) {
-		    Map.Entry<Integer, Road> entry = iterator.next();
-		    entry.getValue().updateRunnableCars();
+			Map.Entry<Integer, Road> entry = iterator.next();
+			entry.getValue().updateRunnableCars();
 		}
 		// step two
-		while(nowWaitedCars.size()!=0) {
-			for(int i=0; i<crossSequence.size(); i++) {
+		while (nowWaitedCars.size() != 0) {
+			for (int i = 0; i < crossSequence.size(); i++) {
 				Cross cross = crosses.get(crossSequence.get(i));
 				cross.updateCross();
 			}
 		}
 		// step three
-		for(int i=0; i<outRoadCars.size(); i++) {
+		for (int i = 0; i < outRoadCars.size(); i++) {
 			Car car = outRoadCars.get(i);
-			if(car.getStartTime()<=nowTime)
-				if(car.startOff())
+			if (car.getStartTime() <= nowTime)
+				if (car.startOff())
 					i--;
 		}
 	}
-	
+
 	public static void printMapSize() {
 		System.out.println("map size: ");
 		System.out.println(roads.size() + " roads");
 		System.out.println(crosses.size() + " crosses");
 		System.out.println(cars.size() + " cars");
 	}
+
+	public static boolean isDispatchFinished() {
+		return finishCars.size() == cars.size();
+	}
+
 }
---
+++ src/com/huawei/handle/GlobalSolver.java
@@ -0,0 +1,36 @@
+package com.huawei.handle;
+
+import org.apache.log4j.Logger;
+import com.huawei.data.RoadMap;
+
+public class GlobalSolver {
+	private static final Logger logger = Logger.getLogger(GlobalSolver.class);
+
+	public GlobalSolver() {
+		// To Do
+	}
+
+	public static void initSolver() {
+		RoadMap.initMap();
+		initCarRoadList();
+	}
+
+	public static void invokeSolver() {
+		initSolver();
+
+		while (!RoadMap.isDispatchFinished()) {
+			RoadMap.updateMap();
+			RoadMap.nowTime++;
+			updateCarRoadList();
+		}
+	}
+
+	// shortest path road sequence
+	public static void initCarRoadList() {
+		// To Do
+	}
+
+	public static void updateCarRoadList() {
+		// To Do
+	}
+}
