a9b4f8b8b68f7a257f9b09f9ddd310a11b32f985
ycnalin <ycnalin@gmail.com>
2019-03-20 10:00:34 +0800

    add method initSortedRoads to GolbalSolver

--- src/com/huawei/service/GlobalSolver.java
+++ src/com/huawei/service/GlobalSolver.java
@@ -1,6 +1,9 @@
 package com.huawei.service;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -12,16 +15,22 @@ import com.huawei.util.MapUtil;
 
 public class GlobalSolver {
 	private static final Logger logger = Logger.getLogger(GlobalSolver.class);
-
-	public GlobalSolver() {
-		// To Do
-	}
-
+	
+	private static LinkedList<ArrayList<Car>> startTimeSortedCars = null;
+	
 	public static void initSolver() {
 		MapSimulator.initMap();
+		//initSortedRoads();
 		initCarRoadList();
+		logger.info("Solver init finished!");
 	}
-
+	
+	/**
+	 * @version: v1.0
+	 * @modified: 2019年3月19日 上午11:10:36
+	 * @description: the only entrance of GlobalSolver class
+	 * @return: a feasible solution directly written to answer.txt
+	 */
 	public static void invokeSolver() {
 		initSolver();
 
@@ -32,12 +41,6 @@ public class GlobalSolver {
 		}
 	}
 
-	
-	
-	
-	
-	
-	
 	
 	// shortest path road sequence
 	public static void initCarRoadList() {
@@ -64,5 +67,47 @@ public class GlobalSolver {
 
 	public static void updateCarRoadList() {
 		// To Do
+	}
+	
+	/**
+	 * @version: v1.0
+	 * @modified: 2019年3月19日 上午11:45:46
+	 * @description: sort roads by startTime and speed
+	 * @return: void
+	 */
+	public static void initSortedRoads() {
+		// first sort road by startTime only
+		ArrayList<Car> sortedCars =  (ArrayList<Car>) MapUtil.cars.values();
+		Collections.sort(sortedCars, new Comparator<Car>() {
+			@Override
+			public int compare(Car o1, Car o2) {
+				if(o1.getStartTime() == o2.getStartTime()) {
+					// ascending sort
+					//return o1.getMaxSpeed() - o2.getMaxSpeed();
+					// descending sort
+					return o2.getMaxSpeed() - o1.getMaxSpeed();
+				}
+				// ascending sort;
+				return o1.getStartTime() - o2.getStartTime();
+			}
+		});
+		
+		startTimeSortedCars = new LinkedList<ArrayList<Car>>();
+		
+		int index = 0, startTime = 0;
+		ArrayList<Car> cars = null;
+		while(index < sortedCars.size()) {
+			cars = new ArrayList<>();
+			cars.add(sortedCars.get(index++));
+			startTime = cars.get(0).getStartTime();
+			
+			while (index < sortedCars.size() && 
+					sortedCars.get(index).getStartTime() == startTime) {
+				cars.add(sortedCars.get(index));
+				++index;
+			}
+			startTimeSortedCars.addLast(cars);
+		}
+		
 	}
 }
